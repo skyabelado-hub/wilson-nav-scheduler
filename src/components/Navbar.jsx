89a973d40c7aa497b22ff98a6ea59fe32dcb0637
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-15 22:20:49 +0800

    Update Navbar component title and list items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 function Navbar() {
   return (
     <nav>
-      <h2>NAVI</h2>
-
+      <h2>WILSON</h2>
       <ul>
         <li>Home</li>
-        <li>Campus Map</li>
-        <li>Appointments</li>
+        <li>Map</li>
+        <li>Appointment</li>
         <li>Login</li>
       </ul>
     </nav>
