c030ac11803b0b81cf129ac9b15a66c953e0d00a
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-15 22:11:47 +0800

    Create Appointment component for booking appointments

---
+++ src/pages/Appointment.jsx
@@ -0,0 +1,20 @@
+function Appointment() {
+  return (
+    <div>
+      <h1>Book Appointment</h1>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Purpose"
+        />
+
+        <button>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Appointment;
