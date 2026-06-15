6b81beaac1a4c53c4a71394baf1fac5af21fc4c9
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-15 22:10:41 +0800

    Add Map component to display campus map

---
+++ src/pages/Map.jsx
@@ -0,0 +1,15 @@
+function Map() {
+  return (
+    <div>
+      <h1>Campus Map</h1>
+
+      <img
+        src="/campus-map.png"
+        alt="Campus Map"
+        width="600"
+      />
+    </div>
+  );
+}
+
+export default Map;
