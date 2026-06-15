91d905d3f6598c3fdb8415e319fa688b79e5837e
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-15 22:12:17 +0800

    Add Login component with email and password inputs

---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+      />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
