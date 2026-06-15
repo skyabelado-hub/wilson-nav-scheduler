2b37d913524440e0d7b6407bd7c88bdd6a150ae5
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-15 22:02:39 +0800

    Add Home component with welcome message

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to NAVI</h1>
+
+      <p>
+        Campus Navigation and Appointment
+        Management System
+      </p>
+    </div>
+  );
+}
+
+export default Home;
