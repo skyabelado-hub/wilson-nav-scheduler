63dd4efed79d0378ce59dc16053e62bb133997b6
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-15 22:21:36 +0800

    Refactor Map component for cleaner markup
    Removed unnecessary line breaks and adjusted alt text.

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -2,12 +2,7 @@ function Map() {
   return (
     <div>
       <h1>Campus Map</h1>
-
-      <img
-        src="/campus-map.png"
-        alt="Campus Map"
-        width="600"
-      />
+      <img src="/campus-map.png" alt="Map" width="600" />
     </div>
   );
 }
